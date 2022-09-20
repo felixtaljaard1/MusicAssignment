90d4d4f0b717538fe8fd6ab4749e62bfce70337e
felixtaljaard1 <felix.taljaard@informationtechconsultants.co.uk>
2022-09-20 15:38:45 +0100

    First androidtest passes

--- app/src/androidTest/java/com/example/musicmvvm/RockFragmentTest.kt
+++ app/src/androidTest/java/com/example/musicmvvm/RockFragmentTest.kt
@@ -6,29 +6,31 @@ import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import com.example.musicmvvm.ui.dashboard.ClassicFragment
+import com.example.musicmvvm.ui.home.RockFragment
 import com.example.musicmvvm.viewmodel.classiclist.ClassicAdapter
 import org.junit.Before
 import org.junit.Test
 
-//class ClassicFragmentTest {
-//
-//    private val navController = TestNavHostController(getApplicationContext())
-//
-//    @Before
-//    fun setUp(){
-//        launchFragmentInHiltContainer<ClassicFragment>(Bundle(), R.style.Theme_MusicMVVM) {
-//            navController.setGraph(R.navigation.mobile_navigation)
-//            Navigation.setViewNavController(requireView(), navController)
-//        }
-//    }
-//
-//    @Test
-//    fun checkIfClassicRecyclerView(){
-//        onView(withId(R.id.recyclerViewClassic))
-//            .check(matches(isDisplayed()))
-//    }
-//}
+class RockFragmentTest {
+
+    private val navController = TestNavHostController(getApplicationContext())
+
+    @Before
+    fun setUp(){
+        launchFragmentInHiltContainer<RockFragment>(Bundle(), R.style.Theme_MusicMVVM) {
+            navController.setGraph(R.navigation.mobile_navigation)
+            Navigation.setViewNavController(requireView(), navController)
+        }
+    }
+
+    @Test
+    fun checkIfClassicRecyclerViewVisible(){
+        onView(withId(R.id.recyclerViewRock))
+            .check(matches(isDisplayed()))
+    }
+}
